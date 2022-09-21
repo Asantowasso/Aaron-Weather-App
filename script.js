b4a279e4a98d124fcfe9f61dbf96926442d46d4d
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-09-20 23:00:45 -0400

    created basic layout

---
+++ script.js
@@ -0,0 +1,5 @@
+// The user will need to be able to search for a city
+// This will return a result of the current and future weather condition
+// The search will produce the name of the city, the date, temperature, wind speed and humidity
+// The application logs the users search history and allows them to click the previously searched cities to view them
+
