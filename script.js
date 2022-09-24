5f44b2735df5bb283d069bf5a0f9e1b9e48a7e9b
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-09-24 12:21:01 -0400

    added API key

--- script.js
+++ script.js
@@ -3,3 +3,14 @@
 // The search will produce the name of the city, the date, temperature, wind speed and humidity
 // The application logs the users search history and allows them to click the previously searched cities to view them
 
+function getApi() {
+var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=94.04&exclude=hourly,daily&appid=b822598e32f666bae94083d5722b3cb8';
+
+}
+
+fetch (requestUrl)
+.then (function(response){
+return response.json();
+})
+
+getApi()
